ad6b40da5131b7705700b7f1773f98ded86b3218
DAWNChiz <akpanchristopher654@gmail.com>
2025-06-21 18:53:49 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,15 @@
+const hre = require("hardhat");
 
+async function main() {
+  const tokenAddress = "0xYourTestTokenAddress"; // Replace this with deployed test token
+  const DawnStaker = await hre.ethers.getContractFactory("DawnStaker");
+  const staker = await DawnStaker.deploy(tokenAddress);
+
+  await staker.deployed();
+  console.log("DawnStaker deployed to:", staker.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
