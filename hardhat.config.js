e7e31aac1d6804cfdddbf6ec10daa9ead4c53434
DAWNChiz <akpanchristopher654@gmail.com>
2025-06-21 19:03:08 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
 
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    helios: {
+      url: "https://rpc-public.testnet.helioschain.network", // Helios testnet RPC
+      accounts: [process.env.PRIVATE_KEY], // store in .env
+    },
+  },
+};
